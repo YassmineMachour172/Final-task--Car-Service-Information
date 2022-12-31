1fe86dee3a59e39c363353c71c30734195733098
MoranLevi <73950497+MoranLevi@users.noreply.github.com>
2022-12-31 23:25:25 +0200

    add background image to home page

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Home.css';
+
+const Home = () => {
+    // const navigate = useNavigate();
+
+    // const handleClick = () => {
+    //     navigate('/insertData');
+    // };
+
+    // return (
+    //     <div id='Home' className='container-fluid img-fluid'>
+    //         test
+    //     </div>
+    // );
+
+    return (
+        <div id='Home' className='backgroundimg'>
+            <div className='container'>
+                <h1>hello</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
