c16b5c57340e530e3881e63f0dc80abb4ff74554
YassmineMachour <74206619+YassmineMachour@users.noreply.github.com>
2023-01-03 23:22:05 +0200

    YASS 2

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 import { useNavigate  } from 'react-router-dom';
 import './Home.css';
+import info from "../../images/info.png";
+import login from "../../images/login.png";
+import register from "../../images/register.png";
+
+
 
 const Home = () => {
     const navigate = useNavigate();
@@ -15,15 +20,30 @@ const Home = () => {
         navigate('/signUp');
     };
 
+    const handleClickAboutUs = () => {
+        navigate('/aboutUs');
+    };
+
     return (
         <div id='Home' className='backgroundimg'>
             <div className='container'>
-                <h1 className='text-center'>Welcome to Car Service Information</h1>
-                <div className='buttons-container'>
-                    <Button className='' onClick={handleClickLogIn}>Log In</Button>
-                    <Button className='' onClick={handleClickSignUp}>Sign Up</Button>
+                <center><h1 className='centered'>Welcome to car service business.</h1></center>
+                <center>
+                <div className='HomeContainer'>
+                    <div>
+                        <img className="icons" src={info}/>
+                        <img className="icons" src={login}/>
+                        <img className="icons" src={register}/>
+                    </div>
+                    <div>
+                        <button type="button"  onClick={handleClickLogIn}>About us</button>
+                        <button type="button"  onClick={handleClickLogIn}>Log In</button>
+                        <button type="button"  onClick={handleClickSignUp}>Sign Up</button>
+                    </div>
                 </div>
+                </center>
             </div>
+            
         </div>
     );
 };
